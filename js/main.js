4c9dec78d3e5a8282555284739dd86c46f263ff0
Maxi Prez <maxi.prez@gmail.com>
2021-09-10 10:41:16 -0300

    seccion nosotros terminada

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // MENU SHOW
 const showMenu = (toggleId, navId) => {
     const toogle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+    nav = document.getElementById(navId);
 
     if(toogle && nav){
         toogle.addEventListener('click', ()=>{
@@ -12,16 +12,16 @@ const showMenu = (toggleId, navId) => {
 showMenu('nav-toggle', 'nav-menu');
 
 // ACTIVE AND REMOVE MENU
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction(){
     //active link
-    navLink.forEach(n => n.classList.remove('nav__active'))
-    this.classList.add('nav__active')
+    navLink.forEach(n => n.classList.remove('nav__active'));
+    this.classList.add('nav__active');
 
     //Remove menu mobile
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show')
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
