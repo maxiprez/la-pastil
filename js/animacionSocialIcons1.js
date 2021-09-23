339278f214912e91250d65c4e2e71a85e33ae653
Maxi Prez <maxi.prez@gmail.com>
2021-09-23 16:52:50 -0300

    se arregló iconos de whatsapp insta scroll y seccion contacto

--- js/animacionSocialIcons1.js
+++ js/animacionSocialIcons1.js
@@ -1,12 +1,17 @@
 /* REMOVE FIXED MEDIA-ICONS (INDEX - NOSOTROS - TYC)*/
-window.addEventListener('scroll', function() {
-   
-    let socialIconsHome =  document.getElementById('home__social-icons');     
+  function acomodarRedesSociales(){
+    let socialIconsHome = document.getElementById('home__social-icons');
     if (window.scrollY >= 900) {
-      socialIconsHome.classList.remove('fixed-1');
-      socialIconsHome.classList.add('media__icons-footer-1');       
+        socialIconsHome.classList.remove('fixed-1');
+        socialIconsHome.classList.add('media__icons-footer-1');
     } else {
-      socialIconsHome.classList.add('fixed-1');
-      socialIconsHome.classList.remove('media__icons-footer-1');        
+        socialIconsHome.classList.add('fixed-1');
+        socialIconsHome.classList.remove('media__icons-footer-1');
     }
-  });
+}
+
+window.addEventListener('scroll', function() {
+    acomodarRedesSociales()
+});
+
+acomodarRedesSociales();
--- js/animacionSocialIcons2.js
+++ js/animacionSocialIcons2.js
@@ -1,12 +1,17 @@
 /* REMOVE FIXED MEDIA-ICONS (CONTACTO - CORPORATIVO - PRODUCTOS - PROMOS - TUTORIALPEDIDOS)*/
-window.addEventListener('scroll', function() {
-   
-    let socialIconsHome =  document.getElementById('scroller__social-icons');     
+  function acomodarRedesSociales(){
+    let socialIconsHome = document.getElementById('scroller__social-icons');
     if (window.scrollY >= 300) {
-      socialIconsHome.classList.remove('fixed-2');
-      socialIconsHome.classList.add('media__icons-footer-2');       
+        socialIconsHome.classList.remove('fixed-2');
+        socialIconsHome.classList.add('media__icons-footer-2');
     } else {
-      socialIconsHome.classList.add('fixed-2');
-      socialIconsHome.classList.remove('media__icons-footer-2');        
+        socialIconsHome.classList.add('fixed-2');
+        socialIconsHome.classList.remove('media__icons-footer-2');
     }
-  });
+}
+
+window.addEventListener('scroll', function() {
+    acomodarRedesSociales()
+});
+
+acomodarRedesSociales();
