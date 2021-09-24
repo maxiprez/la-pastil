da1e232224d179afbea0c1710236736d7c073bbb
Maxi Prez <maxi.prez@gmail.com>
2021-09-24 17:20:44 -0300

    se ahicieron las correcciones de alicia

--- js/animacionSocialIcons1.js
+++ js/animacionSocialIcons1.js
@@ -1,7 +1,7 @@
 /* REMOVE FIXED MEDIA-ICONS (INDEX - NOSOTROS - TYC)*/
   function acomodarRedesSociales(){
     let socialIconsHome = document.getElementById('home__social-icons');
-    if (window.scrollY >= 900) {
+    if (window.scrollY >= 400) {
         socialIconsHome.classList.remove('fixed-1');
         socialIconsHome.classList.add('media__icons-footer-1');
     } else {
